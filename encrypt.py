0b6d678aa3fb745b11bff04775b6a91ca040f5db
Aditya Raute <43912470+dumbcoder7@users.noreply.github.com>
2021-01-14 22:33:39 +0530

    Simplification

--- encrypt.py
+++ encrypt.py
@@ -1,12 +1,3 @@
-def sep(word): 
-    return list(word)
-    
 a = input()
-b = sep(a)
-c = sep(a)
-s = ''
-for i in range(0, len(b)):
-    c[i] = b[len(b) - 1 - i]
-print("Given password: " + s.join(b))
-print("Encrypted as: " + s.join(c))
-    
+print("Given password: " + a)
+print("Encrypted as: " + a[::-1])
